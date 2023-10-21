0b1bd5d8bc950a2e16b03dce526d713810757ff6
Pahom83 <pol_83@mail.ru>
2023-10-21 23:25:58 +0400

    initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,49 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+    private final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicLong count = new AtomicLong(0);
+    private final ConcurrentLinkedDeque<Long> deletedID = new ConcurrentLinkedDeque<>();
+
+    public List<Post> all() {
+        return List.copyOf(posts.values());
+    }
+
+    public Post getById(long id) {
+        return posts.get(id);
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            long id;
+            if (deletedID.size() == 0){
+                count.getAndIncrement();
+                id = count.longValue();
+            } else {
+                id = deletedID.getFirst();
+            }
+            post.setId(id);
+            posts.putIfAbsent(id, post);
+        } else {
+            if (posts.containsKey(post.getId())) {
+                posts.replace(post.getId(), post);
+            }
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (posts.containsKey(id)) {
+            posts.remove(id);
+            deletedID.addLast(id);
+        }
+    }
+}
